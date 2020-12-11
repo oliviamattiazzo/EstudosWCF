a91737ddca7229d402a2e314ac6afdd1114f38dc
Olivia Mattiazzo <oli.pmatt@gmail.com>
2020-12-11 16:08:33 +0000

    Desenvolvimento Interface

---
+++ GerenciadorTarefas/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace GerenciadorTarefas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.txtCalendario = new System.Windows.Forms.MonthCalendar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTarefa = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.lstPendentes = new System.Windows.Forms.CheckedListBox();
+            this.lstConcluidas = new System.Windows.Forms.CheckedListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnLimpar);
+            this.groupBox1.Controls.Add(this.btnInserir);
+            this.groupBox1.Controls.Add(this.txtCalendario);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtTarefa);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(403, 387);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cadastrar nova tarefa";
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(234, 340);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(155, 35);
+            this.btnLimpar.TabIndex = 4;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.Location = new System.Drawing.Point(71, 340);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(155, 35);
+            this.btnInserir.TabIndex = 3;
+            this.btnInserir.Text = "Inserir";
+            this.btnInserir.UseVisualStyleBackColor = true;
+            // 
+            // txtCalendario
+            // 
+            this.txtCalendario.Location = new System.Drawing.Point(71, 76);
+            this.txtCalendario.Name = "txtCalendario";
+            this.txtCalendario.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Data:";
+            // 
+            // txtTarefa
+            // 
+            this.txtTarefa.Location = new System.Drawing.Point(71, 41);
+            this.txtTarefa.Name = "txtTarefa";
+            this.txtTarefa.Size = new System.Drawing.Size(318, 26);
+            this.txtTarefa.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tarefa:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lstPendentes);
+            this.groupBox2.Location = new System.Drawing.Point(421, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(392, 387);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Tarefas pendentes";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.lstConcluidas);
+            this.groupBox3.Location = new System.Drawing.Point(819, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(397, 387);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Tarefas concluídas";
+            // 
+            // lstPendentes
+            // 
+            this.lstPendentes.FormattingEnabled = true;
+            this.lstPendentes.Location = new System.Drawing.Point(6, 41);
+            this.lstPendentes.Name = "lstPendentes";
+            this.lstPendentes.Size = new System.Drawing.Size(380, 319);
+            this.lstPendentes.TabIndex = 0;
+            // 
+            // lstConcluidas
+            // 
+            this.lstConcluidas.FormattingEnabled = true;
+            this.lstConcluidas.Location = new System.Drawing.Point(12, 41);
+            this.lstConcluidas.Name = "lstConcluidas";
+            this.lstConcluidas.Size = new System.Drawing.Size(374, 319);
+            this.lstConcluidas.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1239, 407);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Gerenciador de Tarefas";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.MonthCalendar txtCalendario;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTarefa;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckedListBox lstPendentes;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.CheckedListBox lstConcluidas;
+    }
+}
+
